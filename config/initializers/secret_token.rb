7b6de4dc0e3d7ea6a92b8a4c2b5a5ff39a67cd42
Edwin Vega <edwin.ponc@gmail.com>
2010-08-17 21:32:42 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VentaApp::Application.config.secret_token = '21b9f2fbc69e76d7d65b7ed940dcddab130aa7f3ad4476aa1bad56e74e49e66bee62c5b01f15a7f47f4dc2561a548692b5569d11722cd7456263be427b7835c5'
